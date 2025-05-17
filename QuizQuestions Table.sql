c507a09aec1145113525815ef2f8ca8022da04db
rojinakarki19 <85155088+rojinakarki19@users.noreply.github.com>
2025-05-17 17:08:23 +1000

    Delete QuizQuestions Table.sql

--- QuizQuestions Table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE QuizQuestions (
-    question_id VARCHAR2(6)   NOT NULL,
-    quiz_id VARCHAR2(6)   NOT NULL,
-    question_text TEXT,
-    correct_answer VARCHAR(255),
-PRIMARY KEY(question_id)
-FOREIGN KEY(quiz_id) REFERENCES Quizzes(quiz_id)
-);
